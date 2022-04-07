effb9d5e8151e910c0d64f8a59850e825634a27a
rock619 <rock619@gmail.com>
2022-04-07 19:57:54 +0900

    ✅ math-and-algorithm math_and_algorithm_e 6ms https://atcoder.jp/contests/math-and-algorithm/submissions/30773196

---
+++ math-and-algorithm/math_and_algorithm_e/30773196/Main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const MOD = 100
+
+func solve(N int64, a []int64) {
+	sum := int64(0)
+	for _, v := range a {
+		sum += v
+	}
+	fmt.Println(sum % MOD)
+}
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	const initialBufSize = 4096
+	const maxBufSize = 1000000
+	scanner.Buffer(make([]byte, initialBufSize), maxBufSize)
+	scanner.Split(bufio.ScanWords)
+	var N int64
+	scanner.Scan()
+	N, _ = strconv.ParseInt(scanner.Text(), 10, 64)
+	a := make([]int64, N)
+	for i := int64(0); i < N; i++ {
+		scanner.Scan()
+		a[i], _ = strconv.ParseInt(scanner.Text(), 10, 64)
+	}
+	solve(N, a)
+}
