d5e7dbaa58cf4baf1629331d50c104eb1d2d2c27
rock619 <rock619@gmail.com>
2022-04-07 22:08:30 +0900

    ✅ math-and-algorithm math_and_algorithm_s 46ms https://atcoder.jp/contests/math-and-algorithm/submissions/30775876

---
+++ math-and-algorithm/math_and_algorithm_s/30775876/Main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func solve(N int64, A []int64) {
+	m := make(map[int64]int64, 3)
+	for _, a := range A {
+		m[a]++
+	}
+
+	result := int64(0)
+	for _, v := range m {
+		result += v * (v - 1) / 2
+	}
+	fmt.Println(result)
+}
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	const initialBufSize = 4096
+	const maxBufSize = 1000000
+	scanner.Buffer(make([]byte, initialBufSize), maxBufSize)
+	scanner.Split(bufio.ScanWords)
+	var N int64
+	scanner.Scan()
+	N, _ = strconv.ParseInt(scanner.Text(), 10, 64)
+	A := make([]int64, N)
+	for i := int64(0); i < N; i++ {
+		scanner.Scan()
+		A[i], _ = strconv.ParseInt(scanner.Text(), 10, 64)
+	}
+	solve(N, A)
+}
