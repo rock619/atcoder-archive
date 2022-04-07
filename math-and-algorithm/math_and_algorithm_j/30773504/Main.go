9708fb24dd94b9e48aab9db0e392deafb6c9ecf7
rock619 <rock619@gmail.com>
2022-04-07 20:12:44 +0900

    ✅ math-and-algorithm math_and_algorithm_j 8ms https://atcoder.jp/contests/math-and-algorithm/submissions/30773504

---
+++ math-and-algorithm/math_and_algorithm_j/30773504/Main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func solve(N int64) {
+	product := int64(1)
+	for i := product; i <= N; i++ {
+		product *= i
+	}
+	fmt.Println(product)
+}
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	const initialBufSize = 4096
+	const maxBufSize = 1000000
+	scanner.Buffer(make([]byte, initialBufSize), maxBufSize)
+	scanner.Split(bufio.ScanWords)
+	var N int64
+	scanner.Scan()
+	N, _ = strconv.ParseInt(scanner.Text(), 10, 64)
+	solve(N)
+}
