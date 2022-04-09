4c2709b453b0cf14f76209227b0c59da2a58b42a
rock619 <rock619@gmail.com>
2022-04-09 15:06:23 +0900

    ✅ math-and-algorithm math_and_algorithm_ac 86ms https://atcoder.jp/contests/math-and-algorithm/submissions/30801809

---
+++ math-and-algorithm/math_and_algorithm_ac/30801809/Main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func solve(N int64, A []int64) {
+	results := make([]int64, N+1)
+	for i := int64(0); i <= N; i++ {
+		switch i {
+		case 0:
+			results[i] = 0
+		case 1:
+			results[i] = A[i-1]
+		default:
+			results[i] = Max(results[i-1], results[i-2]+A[i-1])
+		}
+	}
+
+	fmt.Println(results[N])
+}
+
+func Max(a, b int64) int64 {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	const initialBufSize = 4096
+	const maxBufSize = 1000000
+	scanner.Buffer(make([]byte, initialBufSize), maxBufSize)
+	scanner.Split(bufio.ScanWords)
+	var N int64
+	scanner.Scan()
+	N, _ = strconv.ParseInt(scanner.Text(), 10, 64)
+	A := make([]int64, N)
+	for i := int64(0); i < N; i++ {
+		scanner.Scan()
+		A[i], _ = strconv.ParseInt(scanner.Text(), 10, 64)
+	}
+	solve(N, A)
+}
