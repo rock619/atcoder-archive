833a3cdde82f23390bbe7dadb7609b8f976bb369
rock619 <rock619@gmail.com>
2022-04-10 14:11:47 +0900

    ✅ math-and-algorithm math_and_algorithm_aj 50ms https://atcoder.jp/contests/math-and-algorithm/submissions/30837526

---
+++ math-and-algorithm/math_and_algorithm_aj/30837526/Main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func solve(N int64, Q int64, L []int64, R []int64, X []int64) {
+	cells := make([]int64, N)
+	for i := int64(0); i < Q; i++ {
+		cells[L[i]-1] += X[i]
+		if R[i] < N {
+			cells[R[i]] -= X[i]
+		}
+	}
+
+	w := bufio.NewWriter(os.Stdout)
+	defer w.Flush()
+	for i := 1; i < len(cells); i++ {
+		switch {
+		case cells[i] == 0:
+			fmt.Fprint(w, "=")
+		case cells[i] > 0:
+			fmt.Fprint(w, "<")
+		default:
+			fmt.Fprint(w, ">")
+		}
+	}
+	fmt.Fprint(w, "\n")
+}
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	const initialBufSize = 4096
+	const maxBufSize = 1000000
+	scanner.Buffer(make([]byte, initialBufSize), maxBufSize)
+	scanner.Split(bufio.ScanWords)
+	var N int64
+	scanner.Scan()
+	N, _ = strconv.ParseInt(scanner.Text(), 10, 64)
+	var Q int64
+	scanner.Scan()
+	Q, _ = strconv.ParseInt(scanner.Text(), 10, 64)
+	L := make([]int64, Q)
+	R := make([]int64, Q)
+	X := make([]int64, Q)
+	for i := int64(0); i < Q; i++ {
+		scanner.Scan()
+		L[i], _ = strconv.ParseInt(scanner.Text(), 10, 64)
+		scanner.Scan()
+		R[i], _ = strconv.ParseInt(scanner.Text(), 10, 64)
+		scanner.Scan()
+		X[i], _ = strconv.ParseInt(scanner.Text(), 10, 64)
+	}
+	solve(N, Q, L, R, X)
+}
