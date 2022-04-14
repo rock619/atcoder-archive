59271cee3d4de32d0c26d4615c4638d479e9c21b
rock619 <rock619@gmail.com>
2022-04-14 23:08:12 +0900

    ✅ math-and-algorithm math_and_algorithm_au 10ms https://atcoder.jp/contests/math-and-algorithm/submissions/30967462

---
+++ math-and-algorithm/math_and_algorithm_au/30967462/Main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const MOD = 1000000007
+
+type Matrix struct {
+	c [2][2]int64
+}
+
+func (m Matrix) Multiply(m2 Matrix) Matrix {
+	result := Matrix{}
+	for i := 0; i < 2; i++ {
+		for k := 0; k < 2; k++ {
+			for j := 0; j < 2; j++ {
+				result.c[i][j] += m.c[i][k] * m2.c[k][j]
+				result.c[i][j] %= MOD
+			}
+		}
+	}
+	return result
+}
+
+func (m Matrix) Power(n int64) Matrix {
+	p := m
+	q := p
+	flag := false
+	for i := 0; i < 60; i++ {
+		if (n & (1 << i)) != 0 {
+			if !flag {
+				q = p
+				flag = true
+			} else {
+				q = q.Multiply(p)
+			}
+		}
+
+		p = p.Multiply(p)
+	}
+
+	return q
+}
+
+func solve(N int64) {
+	m := Matrix{
+		c: [2][2]int64{
+			{2, 1},
+			{1, 0},
+		},
+	}
+
+	result := m.Power(N - 1)
+	fmt.Println((result.c[1][0] + result.c[1][1]) % MOD)
+}
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	const initialBufSize = 4096
+	const maxBufSize = 1000000
+	scanner.Buffer(make([]byte, initialBufSize), maxBufSize)
+	scanner.Split(bufio.ScanWords)
+	var N int64
+	scanner.Scan()
+	N, _ = strconv.ParseInt(scanner.Text(), 10, 64)
+	solve(N)
+}
