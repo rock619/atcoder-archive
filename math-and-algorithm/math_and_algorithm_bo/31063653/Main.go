c67299c73556b1691cb2d9ae6f1f4f67cea46c5f
rock619 <rock619@gmail.com>
2022-04-17 18:02:38 +0900

    ✅ math-and-algorithm math_and_algorithm_bo 15ms https://atcoder.jp/contests/math-and-algorithm/submissions/31063653

---
+++ math-and-algorithm/math_and_algorithm_bo/31063653/Main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const (
+	YES = "Yes"
+	NO  = "No"
+)
+
+func solve(N int64, X int64, Y int64) {
+	for a := int64(1); a <= N; a++ {
+		for b := a; b <= N; b++ {
+			for c := b; c <= N; c++ {
+				if d := X - (a + b + c); d >= 1 && d <= N && a*b*c*d == Y {
+					fmt.Println(YES)
+					return
+				}
+			}
+		}
+	}
+	fmt.Println(NO)
+}
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	const initialBufSize = 4096
+	const maxBufSize = 1000000
+	scanner.Buffer(make([]byte, initialBufSize), maxBufSize)
+	scanner.Split(bufio.ScanWords)
+	var N int64
+	scanner.Scan()
+	N, _ = strconv.ParseInt(scanner.Text(), 10, 64)
+	var X int64
+	scanner.Scan()
+	X, _ = strconv.ParseInt(scanner.Text(), 10, 64)
+	var Y int64
+	scanner.Scan()
+	Y, _ = strconv.ParseInt(scanner.Text(), 10, 64)
+	solve(N, X, Y)
+}
