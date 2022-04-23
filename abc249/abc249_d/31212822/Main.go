ba246fe7c0eb0fd07995a6b895244fe921d7555d
rock619 <rock619@gmail.com>
2022-04-23 23:10:48 +0900

    ✅ abc249 abc249_d 1192ms https://atcoder.jp/contests/abc249/submissions/31212822

---
+++ abc249/abc249_d/31212822/Main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+)
+
+func solve(N int64, A []int64) {
+	w := bufio.NewWriter(os.Stdout)
+	defer func() {
+		if err := w.Flush(); err != nil {
+			panic(err)
+		}
+	}()
+
+	counts := make(map[int64]int64, N)
+	for _, n := range A {
+		counts[n]++
+	}
+
+	sort.Slice(A, func(i, j int) bool {
+		return A[i] < A[j]
+	})
+
+	sums := make([]int64, N)
+	for i, a := range A {
+		if i > 0 && a == A[i-1] {
+			sums[i] = sums[i-1]
+			continue
+		}
+		for j := int64(1); j*j <= a; j++ {
+			if _, ok := counts[j]; !ok || a%j != 0 {
+				continue
+			}
+			q := a / j
+			if _, ok := counts[q]; !ok {
+				continue
+			}
+
+			if j*j == a {
+				sums[i] += counts[j] * counts[j]
+			} else {
+				sums[i] += 2 * counts[j] * counts[q]
+			}
+		}
+	}
+
+	result := int64(0)
+	for _, s := range sums {
+		result += s
+	}
+	fmt.Fprintln(w, result)
+}
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	const initialBufSize = 4096
+	const maxBufSize = 1048576
+	scanner.Buffer(make([]byte, initialBufSize), maxBufSize)
+	scanner.Split(bufio.ScanWords)
+
+	scanner.Scan()
+	N, _ := strconv.ParseInt(scanner.Text(), 10, 64)
+	A := make([]int64, N)
+	for i := int64(0); i < N; i++ {
+		scanner.Scan()
+		A[i], _ = strconv.ParseInt(scanner.Text(), 10, 64)
+	}
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+
+	solve(N, A)
+}
