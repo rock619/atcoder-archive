e046d15a95c0bf37a44406b137f4b0520c5c1d26
rock619 <rock619@gmail.com>
2022-05-14 09:15:14 +0900

    ✅ typical90 typical90_an 7ms https://atcoder.jp/contests/typical90/submissions/31643503

---
+++ typical90/typical90_an/31643503/Main.go
@@ -0,0 +1,270 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+)
+
+type Pair struct {
+	First, Second int64
+}
+
+type Edge struct {
+	From, To, Cap, Flow int64
+}
+
+type MFGraph struct {
+	n   int64
+	g   [][]edge
+	pos []Pair
+}
+
+func NewMFGraph(n int64) *MFGraph {
+	return &MFGraph{
+		n: n,
+		g: make([][]edge, n),
+	}
+}
+
+func (g *MFGraph) AddEdge(from, to, capacity int64) int64 {
+	m := int64(len(g.pos))
+	g.pos = append(g.pos, Pair{First: from, Second: int64(len(g.g[from]))})
+	fromID := int64(len(g.g[from]))
+	toID := int64(len(g.g[to]))
+	if from == to {
+		toID++
+	}
+	g.g[from] = append(g.g[from], edge{To: to, Rev: toID, Cap: capacity})
+	g.g[to] = append(g.g[to], edge{To: from, Rev: fromID, Cap: 0})
+	return m
+}
+
+func (g *MFGraph) Edge(i int64) Edge {
+	e := g.g[g.pos[i].First][g.pos[i].Second]
+	re := g.g[e.To][e.Rev]
+	return Edge{From: g.pos[i].First, To: e.To, Cap: e.Cap + re.Cap, Flow: re.Cap}
+}
+
+func (g *MFGraph) Edges() []Edge {
+	m := int64(len(g.pos))
+	result := make([]Edge, m)
+	for i := int64(0); i < m; i++ {
+		result[i] = g.Edge(i)
+	}
+	return result
+}
+
+func (g *MFGraph) Change(i, capacity, flow int64) {
+	e := g.g[g.pos[i].First][g.pos[i].Second]
+	g.g[g.pos[i].First][g.pos[i].Second].Cap = capacity - flow
+	g.g[e.To][e.Rev].Cap = flow
+}
+
+func (g *MFGraph) Flow(s, t int64) int64 {
+	return g.FlowLimit(s, t, math.MaxInt64)
+}
+
+func (g *MFGraph) FlowLimit(s, t, limit int64) int64 {
+	flow := int64(0)
+	for flow < limit {
+		level := g.bfs(s, t)
+		if level[t] == -1 {
+			break
+		}
+		iter := make([]int64, g.n)
+		for flow < limit {
+			f := g.dfs(t, s, limit-flow, iter, level)
+			if f == 0 {
+				break
+			}
+			flow += f
+		}
+	}
+	return flow
+}
+
+func (g *MFGraph) bfs(s, t int64) []int64 {
+	level := make([]int64, g.n)
+	for i := range level {
+		level[i] = -1
+	}
+	level[s] = 0
+	que := Queue(make([]int64, 0, 1))
+	que.Enqueue(s)
+	for !que.Empty() {
+		v := que.Dequeue()
+		for _, e := range g.g[v] {
+			if e.Cap == 0 || level[e.To] >= 0 {
+				continue
+			}
+			level[e.To] = level[v] + 1
+			if e.To == t {
+				return level
+			}
+			que.Enqueue(e.To)
+		}
+	}
+	return level
+}
+
+func (g *MFGraph) dfs(cur, s, limit int64, iter, level []int64) int64 {
+	if cur == s {
+		return limit
+	}
+	res := int64(0)
+	curLevel := level[cur]
+	for itMax := int64(len(g.g[cur])); iter[cur] < itMax; iter[cur]++ {
+		i := iter[cur]
+		e := g.g[cur][i]
+		if curLevel <= level[e.To] || g.g[e.To][e.Rev].Cap == 0 {
+			continue
+		}
+		d := g.dfs(e.To, s, Min(limit-res, g.g[e.To][e.Rev].Cap), iter, level)
+		if d <= 0 {
+			continue
+		}
+		g.g[cur][i].Cap += d
+		g.g[e.To][e.Rev].Cap -= d
+		res += d
+		if res == limit {
+			break
+		}
+	}
+	return res
+}
+
+func (g *MFGraph) MinCut(s int64) []bool {
+	visited := make([]bool, g.n)
+	que := Queue(make([]int64, 0, 1))
+	que.Enqueue(s)
+	for !que.Empty() {
+		p := que.Dequeue()
+		visited[p] = true
+		for _, e := range g.g[p] {
+			if e.Cap != 0 && !visited[e.To] {
+				visited[e.To] = true
+				que.Enqueue(e.To)
+			}
+		}
+	}
+	return visited
+}
+
+type edge struct {
+	To, Rev, Cap int64
+}
+
+type Queue []int64
+
+func (q *Queue) Enqueue(v int64) {
+	*q = append([]int64(*q), v)
+}
+
+func (q *Queue) Dequeue() int64 {
+	v := (*q)[0]
+	*q = (*q)[1:]
+	return v
+}
+
+func (q Queue) Size() int64 {
+	return int64(len(q))
+}
+
+func (q Queue) Empty() bool {
+	return q.Size() == 0
+}
+
+func (q *Queue) Clear() {
+	*q = (*q)[:0]
+}
+
+func Min(ints ...int64) int64 {
+	if len(ints) == 0 {
+		panic("Min: len(ints) == 0")
+	}
+	m := ints[0]
+	for i := 1; i < len(ints); i++ {
+		if ints[i] < m {
+			m = ints[i]
+		}
+	}
+	return m
+}
+
+const inf int64 = 1 << 62
+
+func solve(N, W int64, A, k []int64, c [][]int64) {
+	w := bufio.NewWriter(os.Stdout)
+	defer func() {
+		if err := w.Flush(); err != nil {
+			panic(err)
+		}
+	}()
+
+	aSum := int64(0)
+	g := NewMFGraph(N + 2)
+	for i := int64(0); i < N; i++ {
+		aSum += A[i]
+
+		g.AddEdge(0, i+1, A[i])
+		g.AddEdge(i+1, N+1, W)
+		for _, cc := range c[i] {
+			g.AddEdge(cc, i+1, inf)
+		}
+	}
+
+	f := g.Flow(0, N+1)
+	fmt.Fprintln(w, aSum-f)
+}
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	const initialBufSize = 4096
+	const maxBufSize = 1048576
+	scanner.Buffer(make([]byte, initialBufSize), maxBufSize)
+	scanner.Split(bufio.ScanWords)
+
+	scanner.Scan()
+	N, err := strconv.ParseInt(scanner.Text(), 10, 64)
+	if err != nil {
+		panic(err)
+	}
+	scanner.Scan()
+	W, err := strconv.ParseInt(scanner.Text(), 10, 64)
+	if err != nil {
+		panic(err)
+	}
+	A := make([]int64, N)
+	for i := int64(0); i < N; i++ {
+		scanner.Scan()
+		A[i], err = strconv.ParseInt(scanner.Text(), 10, 64)
+		if err != nil {
+			panic(err)
+		}
+	}
+	k := make([]int64, N)
+	c := make([][]int64, N)
+	for i := int64(0); i < N; i++ {
+		scanner.Scan()
+		k[i], err = strconv.ParseInt(scanner.Text(), 10, 64)
+		if err != nil {
+			panic(err)
+		}
+		c[i] = make([]int64, k[i])
+		for j := int64(0); j < k[i]; j++ {
+			scanner.Scan()
+			c[i][j], err = strconv.ParseInt(scanner.Text(), 10, 64)
+			if err != nil {
+				panic(err)
+			}
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+
+	solve(N, W, A, k, c)
+}
