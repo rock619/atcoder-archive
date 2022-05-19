07646eff58ae46bb4b778e95e74336042ae5c3d8
rock619 <rock619@gmail.com>
2022-05-19 09:17:33 +0900

    ✅ typical90 typical90_bc 710ms https://atcoder.jp/contests/typical90/submissions/31790265

---
+++ typical90/typical90_bc/31790265/Main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func solve(N int64, P int64, Q int64, A []int64) {
+	w := bufio.NewWriter(os.Stdout)
+	defer func() {
+		if err := w.Flush(); err != nil {
+			panic(err)
+		}
+	}()
+
+	count := int64(0)
+	for i := int64(0); i < N; i++ {
+		result1 := A[i]
+		for j := i + 1; j < N; j++ {
+			result2 := result1 * A[j] % P
+			for k := j + 1; k < N; k++ {
+				result3 := result2 * A[k] % P
+				for l := k + 1; l < N; l++ {
+					result4 := result3 * A[l] % P
+					for m := l + 1; m < N; m++ {
+						if result5 := result4 * A[m] % P; result5 == Q {
+							count++
+						}
+					}
+				}
+			}
+		}
+	}
+	fmt.Fprintln(w, count)
+}
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	const initialBufSize = 4096
+	const maxBufSize = 1048576
+	scanner.Buffer(make([]byte, initialBufSize), maxBufSize)
+	scanner.Split(bufio.ScanWords)
+
+	var err error
+	scanner.Scan()
+	N, err := strconv.ParseInt(scanner.Text(), 10, 64)
+	if err != nil {
+		panic(err)
+	}
+	scanner.Scan()
+	P, err := strconv.ParseInt(scanner.Text(), 10, 64)
+	if err != nil {
+		panic(err)
+	}
+	scanner.Scan()
+	Q, err := strconv.ParseInt(scanner.Text(), 10, 64)
+	if err != nil {
+		panic(err)
+	}
+	A := make([]int64, N)
+	for i := int64(0); i < N; i++ {
+		scanner.Scan()
+		A[i], err = strconv.ParseInt(scanner.Text(), 10, 64)
+		if err != nil {
+			panic(err)
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+
+	solve(N, P, Q, A)
+}
