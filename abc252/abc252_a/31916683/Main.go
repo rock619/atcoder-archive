9b8a8bbefb766f15884f6467b6a2635139393198
rock619 <rock619@gmail.com>
2022-05-23 20:07:55 +0900

    ✅ abc252 abc252_a 5ms https://atcoder.jp/contests/abc252/submissions/31916683

---
+++ abc252/abc252_a/31916683/Main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func solve(N int64) {
+	w := bufio.NewWriter(os.Stdout)
+	defer func() {
+		if err := w.Flush(); err != nil {
+			panic(err)
+		}
+	}()
+
+	fmt.Fprintf(w, "%s\n", []byte{uint8(N)})
+}
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	const initialBufSize = 4096
+	const maxBufSize = 1048576
+	scanner.Buffer(make([]byte, initialBufSize), maxBufSize)
+	scanner.Split(bufio.ScanWords)
+
+	var err error
+	scanner.Scan()
+	N, err := strconv.ParseInt(scanner.Text(), 10, 64)
+	if err != nil {
+		panic(err)
+	}
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+
+	solve(N)
+}
