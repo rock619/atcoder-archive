a135478174c5601b3380df7487ca8b031050aaca
rock619 <rock619@gmail.com>
2022-06-11 12:23:43 +0900

    ✅ typical90 typical90_ca 4ms https://atcoder.jp/contests/typical90/submissions/32363285

---
+++ typical90/typical90_ca/32363285/Main.go
@@ -0,0 +1,161 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const yes = "Yes"
+const no = "No"
+
+func solve(H, W int, A, B [][]int) {
+	w := bufio.NewWriter(os.Stdout)
+	defer func() {
+		if err := w.Flush(); err != nil {
+			panic(err)
+		}
+	}()
+
+	count := 0
+	for i := range A {
+		if i == H-1 {
+			for j := range A[i] {
+				if A[i][j] != B[i][j] {
+					fmt.Fprintln(w, no)
+					return
+				}
+			}
+			fmt.Fprintln(w, yes)
+			fmt.Fprintln(w, count)
+			return
+		}
+		for j := 0; j < W-1; j++ {
+			diff := B[i][j] - A[i][j]
+			count += Abs(diff)
+			for _, pair := range [][]int{{i, j}, {i + 1, j}, {i, j + 1}, {i + 1, j + 1}} {
+				A[pair[0]][pair[1]] += diff
+			}
+		}
+	}
+}
+
+func Abs(n int) int {
+	if n < 0 {
+		return -n
+	}
+	return n
+}
+
+func main() {
+	s := NewScanner()
+	H := s.Int()
+	W := s.Int()
+	A := s.IntsN(H, W)
+	B := s.IntsN(H, W)
+
+	solve(H, W, A, B)
+}
+
+type Scanner struct {
+	*bufio.Scanner
+}
+
+func NewScanner() *Scanner {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Buffer(make([]byte, 4096), 1048576)
+	scanner.Split(bufio.ScanWords)
+	return &Scanner{
+		Scanner: scanner,
+	}
+}
+
+func (s *Scanner) Int() int {
+	s.Scan()
+	v, err := strconv.Atoi(s.Text())
+	if err != nil {
+		panic(err)
+	}
+	return v
+}
+
+func (s *Scanner) Ints(size int) []int {
+	v := make([]int, size)
+	for i := 0; i < size; i++ {
+		v[i] = s.Int()
+	}
+	return v
+}
+
+func (s *Scanner) Ints2(size int) ([]int, []int) {
+	v1 := make([]int, size)
+	v2 := make([]int, size)
+	for i := 0; i < size; i++ {
+		v1[i] = s.Int()
+		v2[i] = s.Int()
+	}
+	return v1, v2
+}
+
+func (s *Scanner) Ints3(size int) ([]int, []int, []int) {
+	v1 := make([]int, size)
+	v2 := make([]int, size)
+	v3 := make([]int, size)
+	for i := 0; i < size; i++ {
+		v1[i] = s.Int()
+		v2[i] = s.Int()
+		v3[i] = s.Int()
+	}
+	return v1, v2, v3
+}
+
+func (s *Scanner) Ints4(size int) ([]int, []int, []int, []int) {
+	v1 := make([]int, size)
+	v2 := make([]int, size)
+	v3 := make([]int, size)
+	v4 := make([]int, size)
+	for i := 0; i < size; i++ {
+		v1[i] = s.Int()
+		v2[i] = s.Int()
+		v3[i] = s.Int()
+		v4[i] = s.Int()
+	}
+	return v1, v2, v3, v4
+}
+
+func (s *Scanner) IntsN(h, w int) [][]int {
+	v := make([][]int, h)
+	for i := 0; i < h; i++ {
+		v[i] = make([]int, w)
+		for j := 0; j < w; j++ {
+			v[i][j] = s.Int()
+		}
+	}
+	return v
+}
+
+func (s *Scanner) Bytes() []byte {
+	s.Scan()
+	return s.Scanner.Bytes()
+}
+
+func (s *Scanner) BytesN(h int) [][]byte {
+	v := make([][]byte, h)
+	for i := 0; i < h; i++ {
+		v[i] = s.Bytes()
+	}
+	return v
+}
+
+func (s *Scanner) Byte() byte {
+	return s.Bytes()[0]
+}
+
+func (s *Scanner) ByteN(n int) []byte {
+	v := make([]byte, n)
+	for i := 0; i < n; i++ {
+		v[i] = s.Byte()
+	}
+	return v
+}
