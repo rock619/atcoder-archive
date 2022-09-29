3c82caab7203fcb3c0b9d4f8e89884309b25ca77
rock619 <rock619@gmail.com>
2022-09-29 22:14:33 +0900

    ✅ tessoku-book tessoku_book_cb 8ms https://atcoder.jp/contests/tessoku-book/submissions/35235656

---
+++ tessoku-book/tessoku_book_cb/35235656/Main.go
@@ -0,0 +1,182 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+)
+
+const yes = "Yes"
+const no = "No"
+
+func solve(o Printer, N int, A []int) {
+	for i := 0; i < N; i++ {
+		for j := i + 1; j < N; j++ {
+			for k := j + 1; k < N; k++ {
+				if A[i]+A[j]+A[k] == 1000 {
+					o.l(yes)
+					return
+				}
+			}
+		}
+	}
+	o.l(no)
+}
+
+func main() {
+	sc := NewScanner()
+	N := sc.Int()
+	A := make([]int, N)
+	for i := 0; i < N; i++ {
+		A[i] = sc.Int()
+	}
+	out := NewPrinter()
+	solve(out, N, A)
+	if err := out.w.Flush(); err != nil {
+		panic(err)
+	}
+}
+
+type Scanner struct {
+	*bufio.Scanner
+}
+
+func NewScanner() *Scanner {
+	s := bufio.NewScanner(os.Stdin)
+	s.Buffer(make([]byte, 4096), math.MaxInt64)
+	s.Split(bufio.ScanWords)
+	return &Scanner{
+		Scanner: s,
+	}
+}
+
+func (s *Scanner) Scan() {
+	if ok := s.Scanner.Scan(); !ok {
+		panic(s.Err())
+	}
+}
+
+func (s *Scanner) Int() int {
+	s.Scan()
+	v, err := strconv.Atoi(s.Scanner.Text())
+	if err != nil {
+		panic(err)
+	}
+	return v
+}
+
+func (s *Scanner) IntN(n int) []int {
+	v := make([]int, n)
+	for i := 0; i < n; i++ {
+		v[i] = s.Int()
+	}
+	return v
+}
+
+func (s *Scanner) IntN2(n int) ([]int, []int) {
+	v1 := make([]int, n)
+	v2 := make([]int, n)
+	for i := 0; i < n; i++ {
+		v1[i] = s.Int()
+		v2[i] = s.Int()
+	}
+	return v1, v2
+}
+
+func (s *Scanner) IntN3(n int) ([]int, []int, []int) {
+	v1 := make([]int, n)
+	v2 := make([]int, n)
+	v3 := make([]int, n)
+	for i := 0; i < n; i++ {
+		v1[i] = s.Int()
+		v2[i] = s.Int()
+		v3[i] = s.Int()
+	}
+	return v1, v2, v3
+}
+
+func (s *Scanner) IntN4(n int) ([]int, []int, []int, []int) {
+	v1 := make([]int, n)
+	v2 := make([]int, n)
+	v3 := make([]int, n)
+	v4 := make([]int, n)
+	for i := 0; i < n; i++ {
+		v1[i] = s.Int()
+		v2[i] = s.Int()
+		v3[i] = s.Int()
+		v4[i] = s.Int()
+	}
+	return v1, v2, v3, v4
+}
+
+func (s *Scanner) IntNN(h, w int) [][]int {
+	v := make([][]int, h)
+	for i := 0; i < h; i++ {
+		v[i] = make([]int, w)
+		for j := 0; j < w; j++ {
+			v[i][j] = s.Int()
+		}
+	}
+	return v
+}
+
+func (s *Scanner) Bytes() []byte {
+	s.Scan()
+	b := s.Scanner.Bytes()
+	v := make([]byte, len(b))
+	copy(v, b)
+	return v
+}
+
+func (s *Scanner) BytesN(n int) [][]byte {
+	v := make([][]byte, n)
+	for i := 0; i < n; i++ {
+		v[i] = s.Bytes()
+	}
+	return v
+}
+
+func (s *Scanner) Float() float64 {
+	s.Scan()
+	v, err := strconv.ParseFloat(s.Text(), 64)
+	if err != nil {
+		panic(err)
+	}
+	return v
+}
+
+func (s *Scanner) Text() string {
+	s.Scan()
+	return s.Scanner.Text()
+}
+
+type Printer interface {
+	// p fmt.Print
+	p(a ...interface{})
+	// f fmt.Printf
+	f(format string, a ...interface{})
+	// l fmt.Println
+	l(a ...interface{})
+}
+
+type printer struct {
+	w *bufio.Writer
+}
+
+func NewPrinter() *printer {
+	return &printer{bufio.NewWriter(os.Stdout)}
+}
+
+func (p *printer) p(a ...interface{}) {
+	fmt.Fprint(p.w, a...)
+}
+
+func (p *printer) f(format string, a ...interface{}) {
+	fmt.Fprintf(p.w, format, a...)
+}
+
+func (p *printer) l(a ...interface{}) {
+	fmt.Fprintln(p.w, a...)
+}
