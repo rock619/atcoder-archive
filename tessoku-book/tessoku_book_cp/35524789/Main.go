1727348af5fc9aa5596fb711a2ec3c7e3b2651e1
rock619 <rock619@gmail.com>
2022-10-09 13:30:54 +0900

    ✅ tessoku-book tessoku_book_cp 40ms https://atcoder.jp/contests/tessoku-book/submissions/35524789

---
+++ tessoku-book/tessoku_book_cp/35524789/Main.go
@@ -0,0 +1,227 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func solve(o Printer, N int, h []int) {
+	dp := make([]int, N)
+	for i := 1; i < N; i++ {
+		if i == 1 {
+			dp[i] = Abs(h[i] - h[i-1])
+		} else {
+			dp[i] = Min(dp[i-1]+Abs(h[i]-h[i-1]), dp[i-2]+Abs(h[i]-h[i-2]))
+		}
+	}
+
+	result := make([]int, 1, N)
+	result[0] = N
+	for current := N; current != 1; current = result[len(result)-1] {
+		if current == 2 || dp[current-1] == dp[current-1-1]+Abs(h[current-1]-h[current-1-1]) {
+			result = append(result, current-1)
+		} else {
+			result = append(result, current-2)
+		}
+	}
+	Reverse(result)
+	o.l(len(result))
+	o.l(strings.Trim(fmt.Sprint(result), "[]"))
+}
+
+func Abs(n int) int {
+	if n < 0 {
+		return -n
+	}
+	return n
+}
+
+type Element = int
+
+func Reverse(s []Element) {
+	for left, right := 0, len(s)-1; left < right; left, right = left+1, right-1 {
+		s[left], s[right] = s[right], s[left]
+	}
+}
+
+func Min(v ...int) int {
+	switch len(v) {
+	case 0:
+		panic("Min: len(v) == 0")
+	case 1:
+		return v[0]
+	case 2:
+		if v[0] < v[1] {
+			return v[0]
+		}
+		return v[1]
+	default:
+		m := v[0]
+		for i := 1; i < len(v); i++ {
+			if v[i] < m {
+				m = v[i]
+			}
+		}
+		return m
+	}
+}
+
+func main() {
+	sc := NewScanner()
+	N := sc.Int()
+	h := make([]int, N)
+	for i := 0; i < N; i++ {
+		h[i] = sc.Int()
+	}
+	out := NewPrinter()
+	solve(out, N, h)
+	if err := out.w.Flush(); err != nil {
+		panic(err)
+	}
+}
+
+type Scanner struct {
+	*bufio.Scanner
+}
+
+func NewScanner() *Scanner {
+	s := bufio.NewScanner(os.Stdin)
+	s.Buffer(make([]byte, 4096), math.MaxInt64)
+	s.Split(bufio.ScanWords)
+	return &Scanner{
+		Scanner: s,
+	}
+}
+
+func (s *Scanner) Scan() {
+	if ok := s.Scanner.Scan(); !ok {
+		panic(s.Err())
+	}
+}
+
+func (s *Scanner) Int() int {
+	s.Scan()
+	v, err := strconv.Atoi(s.Scanner.Text())
+	if err != nil {
+		panic(err)
+	}
+	return v
+}
+
+func (s *Scanner) IntN(n int) []int {
+	v := make([]int, n)
+	for i := 0; i < n; i++ {
+		v[i] = s.Int()
+	}
+	return v
+}
+
+func (s *Scanner) IntN2(n int) ([]int, []int) {
+	v1 := make([]int, n)
+	v2 := make([]int, n)
+	for i := 0; i < n; i++ {
+		v1[i] = s.Int()
+		v2[i] = s.Int()
+	}
+	return v1, v2
+}
+
+func (s *Scanner) IntN3(n int) ([]int, []int, []int) {
+	v1 := make([]int, n)
+	v2 := make([]int, n)
+	v3 := make([]int, n)
+	for i := 0; i < n; i++ {
+		v1[i] = s.Int()
+		v2[i] = s.Int()
+		v3[i] = s.Int()
+	}
+	return v1, v2, v3
+}
+
+func (s *Scanner) IntN4(n int) ([]int, []int, []int, []int) {
+	v1 := make([]int, n)
+	v2 := make([]int, n)
+	v3 := make([]int, n)
+	v4 := make([]int, n)
+	for i := 0; i < n; i++ {
+		v1[i] = s.Int()
+		v2[i] = s.Int()
+		v3[i] = s.Int()
+		v4[i] = s.Int()
+	}
+	return v1, v2, v3, v4
+}
+
+func (s *Scanner) IntNN(h, w int) [][]int {
+	v := make([][]int, h)
+	for i := 0; i < h; i++ {
+		v[i] = make([]int, w)
+		for j := 0; j < w; j++ {
+			v[i][j] = s.Int()
+		}
+	}
+	return v
+}
+
+func (s *Scanner) Bytes() []byte {
+	s.Scan()
+	b := s.Scanner.Bytes()
+	v := make([]byte, len(b))
+	copy(v, b)
+	return v
+}
+
+func (s *Scanner) BytesN(n int) [][]byte {
+	v := make([][]byte, n)
+	for i := 0; i < n; i++ {
+		v[i] = s.Bytes()
+	}
+	return v
+}
+
+func (s *Scanner) Float() float64 {
+	s.Scan()
+	v, err := strconv.ParseFloat(s.Text(), 64)
+	if err != nil {
+		panic(err)
+	}
+	return v
+}
+
+func (s *Scanner) Text() string {
+	s.Scan()
+	return s.Scanner.Text()
+}
+
+type Printer interface {
+	// p fmt.Print
+	p(a ...interface{})
+	// f fmt.Printf
+	f(format string, a ...interface{})
+	// l fmt.Println
+	l(a ...interface{})
+}
+
+type printer struct {
+	w *bufio.Writer
+}
+
+func NewPrinter() *printer {
+	return &printer{bufio.NewWriter(os.Stdout)}
+}
+
+func (p *printer) p(a ...interface{}) {
+	fmt.Fprint(p.w, a...)
+}
+
+func (p *printer) f(format string, a ...interface{}) {
+	fmt.Fprintf(p.w, format, a...)
+}
+
+func (p *printer) l(a ...interface{}) {
+	fmt.Fprintln(p.w, a...)
+}
