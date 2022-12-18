eb4d29b064e1ca5183a6c91138f87a64ece65d81
rock619 <rock619@gmail.com>
2022-12-18 22:51:23 +0900

    ✅ tessoku-book tessoku_book_ef 52ms https://atcoder.jp/contests/tessoku-book/submissions/37380731

---
+++ tessoku-book/tessoku_book_ef/37380731/Main.go
@@ -0,0 +1,314 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"math/bits"
+	"os"
+	"strconv"
+)
+
+func solve(o Printer, N int, A []int) {
+	segTree := NewSegTree(make([]int, N+1))
+	result := 0
+	for _, a := range A {
+		result += segTree.Prod(a+1, N+1)
+		segTree.Set(a, segTree.Get(a)+1)
+	}
+	o.l(result)
+}
+
+type S = int
+
+func (st *SegTree) op(l, r S) S {
+	return l + r
+}
+
+func (st *SegTree) e() S {
+	return 0
+}
+
+type SegTree struct {
+	n    int
+	log  int
+	size int
+	data []S
+}
+
+func NewSegTree(v []S) *SegTree {
+	st := &SegTree{}
+
+	st.n = len(v)
+	st.log = bits.Len(uint(st.n - 1))
+	st.size = 1 << st.log
+	st.data = make([]S, 2*st.size)
+	for i := range st.data {
+		st.data[i] = st.e()
+	}
+	for i := 0; i < st.n; i++ {
+		st.data[st.size+i] = v[i]
+	}
+	for i := st.size - 1; i >= 1; i-- {
+		st.update(i)
+	}
+	return st
+}
+
+func (st *SegTree) Set(p int, x S) {
+	p += st.size
+	st.data[p] = x
+	for i := 1; i <= st.log; i++ {
+		st.update(p >> i)
+	}
+}
+
+func (st *SegTree) Get(p int) S {
+	return st.data[p+st.size]
+}
+
+func (st *SegTree) Prod(l, r int) S {
+	sml, smr := st.e(), st.e()
+	l += st.size
+	r += st.size
+
+	for l < r {
+		if l&1 != 0 {
+			sml = st.op(sml, st.data[l])
+			l++
+		}
+		if r&1 != 0 {
+			r--
+			smr = st.op(st.data[r], smr)
+		}
+		l >>= 1
+		r >>= 1
+	}
+
+	return st.op(sml, smr)
+}
+
+func (st *SegTree) AllProd() S {
+	return st.data[1]
+}
+
+func (st *SegTree) MaxRight(l int, f func(x S) bool) int {
+	if l == st.n {
+		return st.n
+	}
+
+	l += st.size
+	sm := st.e()
+	for {
+		for l%2 == 0 {
+			l >>= 1
+		}
+		if !f(st.op(sm, st.data[l])) {
+			for l < st.size {
+				l *= 2
+				if f(st.op(sm, st.data[l])) {
+					sm = st.op(sm, st.data[l])
+					l++
+				}
+			}
+			return l - st.size
+		}
+		sm = st.op(sm, st.data[l])
+		l++
+		if l&-l == l {
+			break
+		}
+	}
+	return st.n
+}
+
+func (st *SegTree) MinLeft(r int, f func(x S) bool) int {
+	if r == 0 {
+		return 0
+	}
+
+	r += st.size
+	sm := st.e()
+	for {
+		r--
+		for r > 0 && r%2 != 0 {
+			r >>= 1
+		}
+		if !f(st.op(st.data[r], sm)) {
+			for r < st.size {
+				r *= 2
+				r++
+				if f(st.op(st.data[r], sm)) {
+					sm = st.op(st.data[r], sm)
+					r--
+				}
+			}
+			return r + 1 - st.size
+		}
+		sm = st.op(st.data[r], sm)
+		if r&-r == r {
+			break
+		}
+	}
+	return 0
+}
+
+func (st *SegTree) update(k int) {
+	st.data[k] = st.op(st.data[2*k], st.data[2*k+1])
+}
+
+func main() {
+	sc := NewScanner()
+	N := sc.Int()
+	A := make([]int, N)
+	for i := 0; i < N; i++ {
+		A[i] = sc.Int()
+	}
+	out := NewPrinter()
+	solve(out, N, A)
+	if err := out.w.Flush(); err != nil {
+		panic(err)
+	}
+}
+
+type Scanner struct {
+	*bufio.Scanner
+}
+
+func NewScanner() *Scanner {
+	s := bufio.NewScanner(os.Stdin)
+	s.Buffer(make([]byte, 4096), math.MaxInt64)
+	s.Split(bufio.ScanWords)
+	return &Scanner{
+		Scanner: s,
+	}
+}
+
+func (s *Scanner) Scan() {
+	if ok := s.Scanner.Scan(); !ok {
+		panic(s.Err())
+	}
+}
+
+func (s *Scanner) Int() int {
+	s.Scan()
+	v, err := strconv.Atoi(s.Scanner.Text())
+	if err != nil {
+		panic(err)
+	}
+	return v
+}
+
+func (s *Scanner) IntN(n int) []int {
+	v := make([]int, n)
+	for i := 0; i < n; i++ {
+		v[i] = s.Int()
+	}
+	return v
+}
+
+func (s *Scanner) IntN2(n int) ([]int, []int) {
+	v1 := make([]int, n)
+	v2 := make([]int, n)
+	for i := 0; i < n; i++ {
+		v1[i] = s.Int()
+		v2[i] = s.Int()
+	}
+	return v1, v2
+}
+
+func (s *Scanner) IntN3(n int) ([]int, []int, []int) {
+	v1 := make([]int, n)
+	v2 := make([]int, n)
+	v3 := make([]int, n)
+	for i := 0; i < n; i++ {
+		v1[i] = s.Int()
+		v2[i] = s.Int()
+		v3[i] = s.Int()
+	}
+	return v1, v2, v3
+}
+
+func (s *Scanner) IntN4(n int) ([]int, []int, []int, []int) {
+	v1 := make([]int, n)
+	v2 := make([]int, n)
+	v3 := make([]int, n)
+	v4 := make([]int, n)
+	for i := 0; i < n; i++ {
+		v1[i] = s.Int()
+		v2[i] = s.Int()
+		v3[i] = s.Int()
+		v4[i] = s.Int()
+	}
+	return v1, v2, v3, v4
+}
+
+func (s *Scanner) IntNN(h, w int) [][]int {
+	v := make([][]int, h)
+	for i := 0; i < h; i++ {
+		v[i] = make([]int, w)
+		for j := 0; j < w; j++ {
+			v[i][j] = s.Int()
+		}
+	}
+	return v
+}
+
+func (s *Scanner) Bytes() []byte {
+	s.Scan()
+	b := s.Scanner.Bytes()
+	v := make([]byte, len(b))
+	copy(v, b)
+	return v
+}
+
+func (s *Scanner) BytesN(n int) [][]byte {
+	v := make([][]byte, n)
+	for i := 0; i < n; i++ {
+		v[i] = s.Bytes()
+	}
+	return v
+}
+
+func (s *Scanner) Float() float64 {
+	s.Scan()
+	v, err := strconv.ParseFloat(s.Text(), 64)
+	if err != nil {
+		panic(err)
+	}
+	return v
+}
+
+func (s *Scanner) Text() string {
+	s.Scan()
+	return s.Scanner.Text()
+}
+
+type Printer interface {
+	// p fmt.Print
+	p(a ...interface{})
+	// f fmt.Printf
+	f(format string, a ...interface{})
+	// l fmt.Println
+	l(a ...interface{})
+}
+
+type printer struct {
+	w *bufio.Writer
+}
+
+func NewPrinter() *printer {
+	return &printer{bufio.NewWriter(os.Stdout)}
+}
+
+func (p *printer) p(a ...interface{}) {
+	fmt.Fprint(p.w, a...)
+}
+
+func (p *printer) f(format string, a ...interface{}) {
+	fmt.Fprintf(p.w, format, a...)
+}
+
+func (p *printer) l(a ...interface{}) {
+	fmt.Fprintln(p.w, a...)
+}
