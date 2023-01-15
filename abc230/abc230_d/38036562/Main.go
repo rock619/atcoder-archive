a29759dac1ea6e9e1a4e205e1cacc739931f11e8
rock619 <rock619@gmail.com>
2023-01-15 17:47:31 +0900

    ✅ abc230 abc230_d 126ms https://atcoder.jp/contests/abc230/submissions/38036562

---
+++ abc230/abc230_d/38036562/Main.go
@@ -0,0 +1,234 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+const (
+	// _     = iota
+	debug = iota
+)
+
+type Wall struct {
+	L, R int
+}
+
+func solve(o, lg Printer, N int, D int, L []int, R []int) {
+	walls := make([]Wall, N)
+	for i := range walls {
+		walls[i] = Wall{L: L[i], R: R[i]}
+	}
+	sort.Slice(walls, func(i, j int) bool {
+		if walls[i].R == walls[j].R {
+			return walls[i].L < walls[j].L
+		}
+		return walls[i].R < walls[j].R
+	})
+	lg.f("%#v", walls)
+	count := 0
+	for i := 0; i < N; {
+		count++
+		lg.p(i, count)
+		l := walls[i].R
+		r := l + D - 1
+		for ; i < N && walls[i].L <= r; i++ {
+		}
+	}
+	o.l(count)
+}
+
+func main() {
+	sc := NewScanner()
+	N := sc.Int()
+	D := sc.Int()
+	L := make([]int, N)
+	R := make([]int, N)
+	for i := 0; i < N; i++ {
+		L[i] = sc.Int()
+		R[i] = sc.Int()
+	}
+	stdout := bufio.NewWriter(os.Stdout)
+	out := NewPrinter(stdout)
+	logger := NewLogger()
+	solve(out, logger, N, D, L, R)
+	if err := stdout.Flush(); err != nil {
+		panic(err)
+	}
+}
+
+type Scanner struct {
+	*bufio.Scanner
+}
+
+func NewScanner() *Scanner {
+	s := bufio.NewScanner(os.Stdin)
+	s.Buffer(make([]byte, 4096), math.MaxInt64)
+	s.Split(bufio.ScanWords)
+	return &Scanner{
+		Scanner: s,
+	}
+}
+
+func (s *Scanner) Scan() {
+	if ok := s.Scanner.Scan(); !ok {
+		panic(s.Err())
+	}
+}
+
+func (s *Scanner) Int() int {
+	s.Scan()
+	v, err := strconv.Atoi(s.Scanner.Text())
+	if err != nil {
+		panic(err)
+	}
+	return v
+}
+
+func (s *Scanner) IntN(n int) []int {
+	v := make([]int, n)
+	for i := 0; i < n; i++ {
+		v[i] = s.Int()
+	}
+	return v
+}
+
+func (s *Scanner) IntN2(n int) ([]int, []int) {
+	v1 := make([]int, n)
+	v2 := make([]int, n)
+	for i := 0; i < n; i++ {
+		v1[i] = s.Int()
+		v2[i] = s.Int()
+	}
+	return v1, v2
+}
+
+func (s *Scanner) IntN3(n int) ([]int, []int, []int) {
+	v1 := make([]int, n)
+	v2 := make([]int, n)
+	v3 := make([]int, n)
+	for i := 0; i < n; i++ {
+		v1[i] = s.Int()
+		v2[i] = s.Int()
+		v3[i] = s.Int()
+	}
+	return v1, v2, v3
+}
+
+func (s *Scanner) IntN4(n int) ([]int, []int, []int, []int) {
+	v1 := make([]int, n)
+	v2 := make([]int, n)
+	v3 := make([]int, n)
+	v4 := make([]int, n)
+	for i := 0; i < n; i++ {
+		v1[i] = s.Int()
+		v2[i] = s.Int()
+		v3[i] = s.Int()
+		v4[i] = s.Int()
+	}
+	return v1, v2, v3, v4
+}
+
+func (s *Scanner) IntNN(h, w int) [][]int {
+	v := make([][]int, h)
+	for i := 0; i < h; i++ {
+		v[i] = make([]int, w)
+		for j := 0; j < w; j++ {
+			v[i][j] = s.Int()
+		}
+	}
+	return v
+}
+
+func (s *Scanner) Bytes() []byte {
+	s.Scan()
+	b := s.Scanner.Bytes()
+	v := make([]byte, len(b))
+	copy(v, b)
+	return v
+}
+
+func (s *Scanner) BytesN(n int) [][]byte {
+	v := make([][]byte, n)
+	for i := 0; i < n; i++ {
+		v[i] = s.Bytes()
+	}
+	return v
+}
+
+func (s *Scanner) Float() float64 {
+	s.Scan()
+	v, err := strconv.ParseFloat(s.Text(), 64)
+	if err != nil {
+		panic(err)
+	}
+	return v
+}
+
+func (s *Scanner) Text() string {
+	s.Scan()
+	return s.Scanner.Text()
+}
+
+type Printer interface {
+	// p fmt.Print
+	p(a ...interface{})
+	// f fmt.Printf
+	f(format string, a ...interface{})
+	// l fmt.Println
+	l(a ...interface{})
+}
+
+type printer struct {
+	w io.Writer
+}
+
+func NewPrinter(w io.Writer) Printer {
+	return &printer{w}
+}
+
+func (p *printer) p(a ...interface{}) {
+	fmt.Fprint(p.w, a...)
+}
+
+func (p *printer) f(format string, a ...interface{}) {
+	fmt.Fprintf(p.w, format, a...)
+}
+
+func (p *printer) l(a ...interface{}) {
+	fmt.Fprintln(p.w, a...)
+}
+
+type logger struct {
+	*log.Logger
+}
+
+func NewLogger() Printer {
+	return &logger{
+		log.New(os.Stderr, "", log.Lmicroseconds|log.Lshortfile),
+	}
+}
+
+func (l *logger) p(a ...interface{}) {
+	if debug == 1 {
+		l.Logger.Print(a...)
+	}
+}
+
+func (l *logger) f(format string, a ...interface{}) {
+	if debug == 1 {
+		l.Logger.Printf(format, a...)
+	}
+}
+
+func (l *logger) l(a ...interface{}) {
+	if debug == 1 {
+		l.Logger.Println(a...)
+	}
+}
